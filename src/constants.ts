88cb929f8ee0bcfa92f939d01596b657cf96bff7
Matheus <53306408+mathsue0@users.noreply.github.com>
2026-03-04 17:16:05 -0300

    fix: update SAFEFY_PAYMENT_API_BASE_URL to use HTTPS

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SAFEFY_PAYMENT_API_BASE_URL = "http://api-payment.safefypay.com.br";
+export const SAFEFY_PAYMENT_API_BASE_URL = "https://api-payment.safefypay.com.br";
 export const SAFEFY_STATUS_PAGE_URL = "https://status.safefypay.com.br/";
